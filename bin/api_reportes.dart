8d751ee0965b1d08ab507c1be7fe8a45c46e7de8
carlosdc12 <cgalindo_23@alu.uabcs.mx>
2025-11-03 13:58:56 -0700

    ACT 1

--- bin/api_reportes.dart
+++ bin/api_reportes.dart
@@ -1,13 +1,36 @@
+import 'dart:convert';
 import 'package:shelf/shelf.dart';
 import 'package:shelf/shelf_io.dart' as io;
 import 'package:shelf_router/shelf_router.dart';
 
 void main() async {
   final router = Router();
+  final reports = [];
+  final users = [
+    {"name": "Angel", "age": 35, "phone": "6121234567"},
+    {"name": "Jesus", "age": 55, "phone": "6121234569"},
+  ];
 
   router.get('/hello', (Request request) {
     return Response.ok('Hello, World:');
   });
+
+  router.get('/reports', (Request request) {
+    final jsonResponse = jsonEncode(reports);
+    return Response.ok(
+      jsonResponse,
+      headers: {'content-type': 'application/json'},
+    );
+  });
+
+  router.get('/users', (Request request) {
+    final jsonResponse = jsonEncode(users);
+    return Response.ok(
+      jsonResponse,
+      headers: {'content-type': 'application/json'},
+    );
+  });
+
   final handler = const Pipeline()
       .addMiddleware(logRequests())
       .addHandler(router);
